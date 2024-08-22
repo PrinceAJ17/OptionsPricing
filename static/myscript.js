f901a7f1f28b3472709a4a3f06b3074bd293e8cd
Prince Alexander John <princeajohn2005@gmail.com>
2024-08-22 05:58:54 +0400

    Simplified rounding off

--- static/myscript.js
+++ static/myscript.js
@@ -1,15 +1,10 @@
 const form = document.getElementById('formSubmission');
 const inputs = form.querySelectorAll('input[type="number"]');
-let debounceTimeout;
 
 inputs.forEach(element => {
-    element.addEventListener('input', () => {
-        clearTimeout(debounceTimeout);
-        if (element.value.length >= 4) {
-            debounceTimeout = setTimeout(() => {
-                let valueOfInput = parseFloat(element.value);
-                element.value = valueOfInput.toFixed(2);
-            }, 600); 
-        }
-    });
-});
+    element.addEventListener("change",()=>{
+        value = parseFloat(element.value)
+        value = value.toFixed(2)
+        element.value = value
+    })
+})
