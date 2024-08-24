1f389cb2e3dd3ee2eca6639d937623762ede9c96
Prince Alexander John <princeajohn2005@gmail.com>
2024-08-24 21:22:59 +0400

    Started first steps of making heatmap

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request
-import numpy
+import numpy as np
+import seaborn as sns
 from scipy.stats import norm
+import matplotlib.pyplot as plt  # Import matplotlib for plotting
 
 
-app=  Flask(__name__)
+
+
+app = Flask(__name__)
 
 def call(S,X,T,r,sigma):
-    d1 = (numpy.log(S/X)+(r+((sigma)**2)/2)*T)/(sigma*numpy.sqrt(T))
-    d2 = d1 - (sigma*numpy.sqrt(T))
+    d1 = (np.log(S/X)+(r+((sigma)**2)/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - (sigma*np.sqrt(T))
 
-    C = S*norm.cdf(d1) - ((X*numpy.exp(-r*T))*norm.cdf(d2))
+    C = S*norm.cdf(d1) - ((X*np.exp(-r*T))*norm.cdf(d2))
     return C
 
 def put(S,X,T,r,sigma):
-    d1 = (numpy.log(S/X)+(r+((sigma)**2)/2)*T)/(sigma*numpy.sqrt(T))
-    d2 = d1 - (sigma*numpy.sqrt(T))
+    d1 = (np.log(S/X)+(r+((sigma)**2)/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - (sigma*np.sqrt(T))
 
-    P = ((X*numpy.exp(-r*T))*norm.cdf(-d2)) - (S*norm.cdf(-d1))
+    P = ((X*np.exp(-r*T))*norm.cdf(-d2)) - (S*norm.cdf(-d1))
     return P
 
 def getFormValue(form, nameOfInput, defaultValue):
@@ -89,7 +93,42 @@ def calc():
         maxVolatility=f"{maxVol:.2f}"
     )
 
+@app.route("/heatmap", methods=["POST"])
+def heatmap():
+    S = getFormValue(request.form,"stockPrice", 150.00)
+    X = getFormValue(request.form,"strikePrice", 100.00)
+    r = getFormValue(request.form,"RFIR", 0.05)
+    T = getFormValue(request.form,"expiration", 1.00)
+    sigma = getFormValue(request.form,"volatility",0.20)
 
+    call_price = call(S, X, T, r, sigma)
+    put_price = put(S, X, T, r, sigma)
+
+    MinSP = getFormValue(request.form,"minStockPrice",S*0.8)
+    MaxSP = getFormValue(request.form,"maxStockPrice",S*1.2)
+
+    minVol = getFormValue(request.form,"minVolatility", sigma*0.5)
+    maxVol = getFormValue(request.form,"maxVolatility", sigma*1.5)
+
+    stockRange = np.round(np.linspace(MinSP, MaxSP, 10),2)
+    volRange = np.round(np.linspace(minVol, maxVol, 10),2)
+
+
+    call_prices = np.zeros((len(stockRange), len(volRange)))
+    put_prices = np.zeros((len(stockRange), len(volRange)))
+
+    for i, vol in enumerate(volRange):
+        for j, stock in enumerate(stockRange):
+            call_prices[j, i] = round(call(stock, vol),2)
+            put_prices[j, i] = round(put(stock, vol),2)
+
+# Create heatmap for call prices
+    plt.figure(figsize=(10, 8))  # Set the figure size
+    sns.heatmap(call_prices, cmap="Blues", annot=True, xticklabels=stockRange, yticklabels=volRange)
+    plt.title('Call Prices Heatmap')
+    plt.xlabel('Stock Price')
+    plt.ylabel('Volatility')
+    plt.show() 
 
 if __name__ == "__main__":
     app.run(debug=True)
